00bd921f5b4cd8f4310b6e14617a6e1684b66400
RubenFernandezS <139725611+RubenFernandezS@users.noreply.github.com>
2024-02-11 15:59:53 -0600

    Add files via upload

---
+++ Estudiantes.Entidades/Estudiantes.Entidades/MessageSocket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudiantes.Entidades
+{
+    [Serializable]
+    public class MessageSocket<T>
+    {
+        public string Method { get; set; }
+        public T Entity { get; set; }
+    }
+}
---
+++ Estudiantes.Entidades/Estudiantes.Entidades/Teacher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudiantes.Entidades
+{
+    public class Teacher: Person
+    {
+        public string User { get; set; }
+        public string Pass { get; set; }
+
+        public List<Student> Students { get; set; }
+
+        public Teacher()
+        {
+            Students = new List<Student>();
+        }
+
+        public bool AddStudent(Student student)
+        {
+            try
+            {
+                Students.Add(student);
+                return true;
+            }catch (Exception ex)
+            {
+                return false;
+            }
+            
+        }
+    }
+}
